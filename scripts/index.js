561b2235afe59a66114eaf214d743a6584abadb7
RR <roman.box.009@yandex.ru>
2022-01-17 22:55:03 +0300

    refactor: change js popup function

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,37 @@ let btnOpenPopupEditProfile = document.querySelector('.profile__edit-btn');
 let btnClosePopupEditProfile = document.querySelector('.popup-edit-prof__btn-close');
 let btnSubmitPopupEditProfile = document.querySelector('.popup-edit-prof__btn-submit');
 let formEditProfile = document.querySelector('.popup-edit-prof__form');
+let popupEditProfile = document.querySelector('.popup-edit-prof');
+let inputNameEditProfile = document.querySelector('.popup-edit-prof__input-name');
+let inputJobEditProfile = document.querySelector('.popup-edit-prof__input-job');
 
 // Функция сохранения изменений
 function funcSubmitEditProfile() {
-  document.querySelector('.profile__name').textContent = document.querySelector('.popup-edit-prof__input-name').value;
-  document.querySelector('.profile__job').textContent = document.querySelector('.popup-edit-prof__input-job').value;
-  document.querySelector('.popup-edit-prof').classList.remove('popup-edit-prof_opened');  
+  console.log('save');
+  console.log(inputNameEditProfile.value);
+  document.querySelector('.profile__name').textContent = inputNameEditProfile.value;
+  document.querySelector('.profile__job').textContent = inputJobEditProfile.value;
+  popupEditProfile.classList.remove('popup-edit-prof_opened');  
 }
 
 // Функция открытия окна редактирования профиля
 function funcOpenEditProfile() {
-  document.querySelector('.popup-edit-prof').classList.add('popup-edit-prof_opened'); 
+  popupEditProfile.classList.add('popup-edit-prof_opened'); 
 }
 
 // Функция закрытия окна редактирования профиля
 function funcCloseEditProfile() {
-  document.querySelector('.popup-edit-prof').classList.remove('popup-edit-prof_opened'); 
+  popupEditProfile.classList.remove('popup-edit-prof_opened'); 
 }
 
 // обработчики событий
-btnClosePopupEditProfile.addEventListener('click', funcSubmitEditProfile);
+btnClosePopupEditProfile.addEventListener('click', funcCloseEditProfile);
 
-btnSubmitPopupEditProfile.addEventListener('click', funcCloseEditProfile);
+btnSubmitPopupEditProfile.addEventListener('click', funcSubmitEditProfile);
 
 btnOpenPopupEditProfile.addEventListener('click', funcOpenEditProfile);
 
-formEditProfile.addEventListener('keydown', function(key) {
+popupEditProfile.addEventListener('keydown', function(key) {
   if (key.keyCode === 13) {
     funcSubmitEditProfile();
   } else if (key.keyCode === 27) {
