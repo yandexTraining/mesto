14ae64c0c5a35194098e9b46efd542c64fe11207
RR <roman.box.009@yandex.ru>
2022-01-24 02:07:39 +0300

    fix: fix the issues after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,65 @@
+let profilePopup = document.querySelector('.popup-edit-prof');
+let profilePopupOpenBtn = document.querySelector('.profile__edit-btn');
+let profilePopupCloseBtn = document.querySelector('.popup-edit-prof__btn-close');
+let profilePopupInputName = document.getElementById('input-name');
+let profilePopupInputJob = document.getElementById('input-job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+
+profilePopupOpenBtn.addEventListener('click', funcProfilePopupOpen);
+profilePopupCloseBtn.addEventListener('click', funcProfilePopupClose);
+profilePopup.addEventListener('submit', funcProfilePopupSubmit);
+
+
+function funcProfilePopupOpen() {
+  profilePopup.classList.add('popup-edit-prof_opened');
+  profilePopupInputName.value = profileName.textContent;
+  profilePopupInputJob.value = profileJob.textContent;
+}
+
+function funcProfilePopupClose() {
+  profilePopup.classList.remove('popup-edit-prof_opened');
+}
+
+function funcProfilePopupSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profilePopupInputName.value;
+  profileJob.textContent = profilePopupInputJob.value;
+  funcProfilePopupClose();
+}
+
+
+
+/*
 let btnOpenPopupEditProfile = document.querySelector('.profile__edit-btn');
 let btnClosePopupEditProfile = document.querySelector('.popup-edit-prof__btn-close');
 let btnSubmitPopupEditProfile = document.querySelector('.popup-edit-prof__btn-submit');
 let formEditProfile = document.querySelector('.popup-edit-prof__form');
 let popupEditProfile = document.querySelector('.popup-edit-prof');
-let inputNameEditProfile = document.querySelector('.popup-edit-prof__input-name');
-let inputJobEditProfile = document.querySelector('.popup-edit-prof__input-job');
+let inputNameEditProfile = document.getElementById('input-name');
+let inputJobEditProfile = document.getElementById('input-job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+
+
+btnOpenPopupEditProfile.addEventListener('click', funcOpenEditProfile);
+
+btnClosePopupEditProfile.addEventListener('click', funcCloseEditProfile);
+
+btnSubmitPopupEditProfile.addEventListener('click', funcSubmitEditProfile);
+
+
+// Функция открытия окна редактирования профиля
+function funcOpenEditProfile() {
+  popupEditProfile.classList.add('popup-edit-prof_opened');
+  inputNameEditProfile.setAttribute('value', profileName) 
+  inputJobEditProfile.setAttribute('value', profileJob)  
+}
+
+
+
 
 // Функция сохранения изменений
 function funcSubmitEditProfile() {
@@ -15,29 +70,9 @@ function funcSubmitEditProfile() {
   popupEditProfile.classList.remove('popup-edit-prof_opened');  
 }
 
-// Функция открытия окна редактирования профиля
-function funcOpenEditProfile() {
-  popupEditProfile.classList.add('popup-edit-prof_opened'); 
-}
 
 // Функция закрытия окна редактирования профиля
 function funcCloseEditProfile() {
   popupEditProfile.classList.remove('popup-edit-prof_opened'); 
 }
-
-// обработчики событий
-btnClosePopupEditProfile.addEventListener('click', funcCloseEditProfile);
-
-btnSubmitPopupEditProfile.addEventListener('click', funcSubmitEditProfile);
-
-btnOpenPopupEditProfile.addEventListener('click', funcOpenEditProfile);
-
-popupEditProfile.addEventListener('keydown', function(key) {
-  if (key.keyCode === 13) {
-    funcSubmitEditProfile();
-  } else if (key.keyCode === 27) {
-    funcCloseEditProfile();
-  }
-});
-
-
+*/
